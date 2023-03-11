57c90ec2d68dad77d6a224b213bbb3f46308b6a0
Thacio Alves <118781681+thacioalves@users.noreply.github.com>
2023-03-10 21:39:14 -0300

    Implementação JPA e DTO

---
+++ src/main/java/br/unitins/repository/BrinquedoRepository.java
@@ -0,0 +1,19 @@
+package br.unitins.repository;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import br.unitins.model.Brinquedo;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class BrinquedoRepository implements PanacheRepository<Brinquedo> {
+    
+    public List<Brinquedo> findByNome(String nome) {
+        if (nome == null)
+            return null;
+        return find("UPPER(nome) LIKE ?1 ", "%" + nome.toUpperCase() + "%").list();
+    }
+
+}
